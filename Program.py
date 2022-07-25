292cf73e3ed5f7f82e5c79f000a188d417f4f418
LuisGabrielFeitosa <luisgabrielsf6@gmail.com>
2022-07-25 00:03:15 -0300

    Programa finalizado e atualizado com funções

--- Program.py
+++ Program.py
@@ -1,43 +1,81 @@
-import os
+def InformarPais(cod):
+    pais_loc = "";
 
+    if(cod == "789" or cod == "790"):
+        pais_loc = "Brasil";        
+
+    elif(cod == "779"):
+        pais_loc = "Argentina";
+        
+
+    elif(cod == "773"):
+        pais_loc = "Uruguai";
+        
+
+    elif(cod == "780"):
+        pais_loc = "Chile";       
+
+    elif(cod == "784"):
+        pais_loc = "Paraguai";
+                
+    elif(cod == "786"):
+        pais_loc = "Equador";
+        
+    elif(cod == "770"):
+        pais_loc = "Colômbia";
+        
+    elif(cod == "743"):
+        pais_loc = "Nicarágua";
+        
+    elif(cod == "600" or cod == "601"):
+        pais_loc = "África do Sul";
+        
+    else:
+        pais_loc = "VAREJOS S/A não vende para este país: ";        
+    
+    return pais_loc;
+
+def Titulo():
+    print("-------------------------------");
+    print("         VAREJOS S/A           ");
+    print("   Código GTIN-13/UPC/EAN-13   ");
+    print("-------------------------------");
+
+import os
 continuar = True;
 
 os.system('cls');
 
 while(continuar):
     soma = 0;
+    aux1 = 0;
+    aux2 = 0;
 
-    print("-------------------------------");
-    print("         VAREJOS S/A           ");
-    print("   Código GTIN-13/UPC/EAN-13   ");
-    print("-------------------------------");
+    Titulo();
     print("1)Validar código GTIN-13       ");
     print("2)Identificar País             ");
     print("3)Sair                         ");
     print("-------------------------------");
     
-    op = int(input('Digite a opção: '));
+    op = int(input("Digite a opção: "));
     os.system('cls');
 
-    print("-------------------------------");
-    print("         VAREJOS S/A           ");
-    print("   Código GTIN-13/UPC/EAN-13   ");
-    print("-------------------------------");
+    Titulo();
     
 # Código da opção 1
     if(op == 1):
         print("VALIDAR CÓDIGO");
-        codigo13 = str(input('Digite o código de 13 digitos: '));
+        codigo13 = str(input("Digite o código de 13 digitos: "));
         
         if(len(codigo13) != 13):
             print("Número GTIN-13 não possui 13 digitos!");
             input("Pressione ENTER para continuar...");
             os.system('cls');
         
-        else:            
+        else:
             for i in range(0, 12, 2):
-                aux1 = int(codigo13[i]) * 1;
-                aux2 = int(codigo13[i+1]) * 3;                
+                aux1 += int(codigo13[i]) * 1;
+                aux2 += int(codigo13[i+1]) * 3;
                 soma += aux1 + aux2;
 
             divisao = soma // 10;
@@ -62,42 +100,23 @@ while(continuar):
 # Código da opção 2
     elif(op == 2):
         print("IDENTIFICAR PAÍS");
-        codigo13 = input('Digite o código de 13 digitos: ');
-        codPais = codigo13[:3];
-
-        if(codPais == 789 or codPais == 790):
-            pais = "Brasil";
-
-        elif(codPais == 779):
-            pais = "Argentina";
-
-        elif(codPais == 773):
-            pais = "Uruguai";
-
-        elif(codPais == 780):
-            pais = "Chile";
-
-        elif(codPais == 784):
-            pais = "Paraguai";
+        codigo13 = input("Digite o código de 13 digitos: ");
         
-        elif(codPais == 786):
-            pais = "Equador";
-        
-        elif(codPais == 770):
-            pais = "Colômbia";
-        
-        elif(codPais == 743):
-            pais = "Nicarágua";
-        
-        elif(codPais == 600 or 601):
-            pais = "África do Sul";
+        if(len(codigo13) != 13):
+            print("Número GTIN-13 não possui 13 digitos!");
+            input("Pressione ENTER para continuar...");
+            os.system('cls');
 
         else:
-            print("VAREJOS S/A não vende para este país: ", codPais);
-            input("Pressione ENTER para continuar...");
-        
-        print("GTIN-13 origem %s: %s" %(pais, codPais));
-        input("Pressione ENTER para continuar...");
+            codPais = codigo13[:3];
+            pais = InformarPais(codPais);
+
+            if "VAREJOS S/A não" not in pais:
+                print("GTIN-13 origem %s: %s" %(pais, codPais));
+                input("Pressione ENTER para continuar...");
+            else:
+                print(pais, codPais);
+                input("Pressione ENTER para continuar...");
     
 # Código da opção 3
     elif(op == 3):
